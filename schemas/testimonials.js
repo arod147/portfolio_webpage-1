96b1c3479ad8dd30165b7802aeefbd513a76f602
ggaiti <ggaiti@hotmail.com>
2023-08-10 20:46:23 -0400

    setting up sanity, first push

---
+++ schemas/testimonials.js
@@ -0,0 +1,30 @@
+export default {
+  name: 'testimonials',
+  title: 'Testimonials',
+  type: 'documents',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'company',
+      title: 'Company',
+      type: 'string',
+    },
+    {
+      name: 'imageurl',
+      title: 'Imageurl',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'feedback',
+      title: 'Feedback',
+      type: 'string',
+    },
+  ],
+}
